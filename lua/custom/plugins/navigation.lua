ed260affad759f4532b445406360136ab4bef5f1
James Backhouse <james.backhouse@novata.com>
2025-03-27 11:07:08 +0000

    Add folke/flash.

--- lua/custom/plugins/navigation.lua
+++ lua/custom/plugins/navigation.lua
@@ -1,25 +1,45 @@
 return {
   {
     'aaronik/treewalker.nvim',
-    -- The following options are the defaults.
-    -- Treewalker aims for sane defaults, so these are each individually optional,
-    -- and the whole opts block is optional as well.
     opts = {
-      -- Whether to briefly highlight the node after jumping to it
       highlight = true,
-
-      -- How long should above highlight last (in ms)
       highlight_duration = 250,
-
-      -- The color of the above highlight. Must be a valid vim highlight group.
-      -- (see :h highlight-group for options)
       highlight_group = 'ColorColumn',
     },
+  },
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {
+      jump = {
+        nohlsearch = true,
+      },
+      modes = {
+        search = {
+          enabled = true,
+          highlight = {
+            backdrop = true,
+          },
+        },
+        char = {
+          jump_labels = true,
+        },
+      },
+    },
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
   },
 
   vim.keymap.set({ 'n', 'v' }, '<D-j>', '<cmd>Treewalker Down<CR>', { noremap = true }),
   vim.keymap.set({ 'n', 'v' }, '<D-k>', '<cmd>Treewalker Up<CR>', { noremap = true }),
-  vim.keymap.set({ 'n', 'v' }, '<D-h>', '<cmd>Treewalker Left<CR>', { noremap = true }),
+  vim.keymap.set({ 'n', 'v' }, '<D-g>', '<cmd>Treewalker Left<CR>', { noremap = true }),
   vim.keymap.set({ 'n', 'v' }, '<D-l>', '<cmd>Treewalker Right<CR>', { noremap = true }),
   vim.keymap.set('n', '<D-S-j>', '<cmd>Treewalker SwapDown<CR>', { noremap = true }),
   vim.keymap.set('n', '<D-S-k>', '<cmd>Treewalker SwapUp<CR>', { noremap = true }),
