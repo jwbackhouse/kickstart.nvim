e64d780d7d93b722f5a4912079b476838e76889a
James Backhouse <james.backhouse@novata.com>
2025-05-13 19:47:41 +0100

    Swap Harpoon for Grapple.

--- init.lua
+++ init.lua
@@ -348,39 +348,6 @@ require('lazy').setup({
       }
     end,
   },
-  {
-    'ThePrimeagen/harpoon',
-    branch = 'harpoon2',
-    config = function()
-      local harpoon = require 'harpoon'
-      ---@diagnostic disable-next-line: missing-parameter
-      harpoon:setup()
-      local function map(lhs, rhs, opts)
-        vim.keymap.set('n', lhs, rhs, opts or {})
-      end
-      map('<leader>ha', function()
-        harpoon:list():add()
-      end, { desc = '[H]arpoon [A]ppend' })
-      map('<leader>hh', function()
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end, { desc = '[H]arpoon [H]arpoon' })
-      map('<leader>1', function()
-        harpoon:list():select(1)
-      end, { desc = '[H]arpoon [1]' })
-      map('<leader>2', function()
-        harpoon:list():select(2)
-      end, { desc = '[H]arpoon [2]' })
-      map('<leader>3', function()
-        harpoon:list():select(3)
-      end, { desc = '[H]arpoon [3]' })
-      map('<leader>4', function()
-        harpoon:list():select(4)
-      end, { desc = '[H]arpoon [4]' })
-      map('<leader>5', function()
-        harpoon:list():select(5)
-      end, { desc = '[H]arpoon [5]' })
-    end,
-  },
 
   -- Original
   -- {
---
+++ lua/custom/plugins/sessions.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    'cbochs/grapple.nvim',
+    dependencies = {
+      { 'nvim-tree/nvim-web-devicons', lazy = true },
+    },
+    opts = {
+      scope = 'git_branch',
+      win_opts = {
+        width = 120,
+      },
+    },
+    event = { 'BufReadPost', 'BufNewFile' },
+    cmd = 'Grapple',
+    keys = {
+      { '<leader>st', '<cmd>Grapple toggle<cr>', desc = '[S]ession [T]ag File' },
+      { '<leader>sw', '<cmd>Grapple toggle_tags<cr>', desc = '[S]ession Toggle [W]indow' },
+      { '<leader>sl', '<cmd>Grapple toggle scope=global<cr>', desc = '[S]ession Tag File G[L]obal' },
+      { '<leader>sg', '<cmd>Grapple toggle_tags scope=global<cr>', desc = '[S]ession Toggle [G]lobal Window' },
+      { '<leader>so', '<cmd>Grapple cycle_tags next<cr>', desc = '[S]ession cycle previous tag [p]' },
+      { '<leader>si', '<cmd>Grapple cycle_tags prev<cr>', desc = '[S]ession cycle next tag [i]' },
+
+      { '<leader>1', '<cmd>Grapple select index=1<cr>', desc = 'Session [1]' },
+      { '<leader>2', '<cmd>Grapple select index=2<cr>', desc = 'Session [2]' },
+      { '<leader>3', '<cmd>Grapple select index=3<cr>', desc = 'Session [3]' },
+      { '<leader>4', '<cmd>Grapple select index=4<cr>', desc = 'Session [4]' },
+      { '<leader>5', '<cmd>Grapple select index=5<cr>', desc = 'Session [5]' },
+
+      { '<leader>6', '<cmd>Grapple select index=1 scope=global <cr>', desc = 'Session Global [1]' },
+      { '<leader>7', '<cmd>Grapple select index=2 scope=global <cr>', desc = 'Session Global [2]' },
+      { '<leader>8', '<cmd>Grapple select index=3 scope=global <cr>', desc = 'Session Global [3]' },
+      { '<leader>9', '<cmd>Grapple select index=4 scope=global <cr>', desc = 'Session Global [4]' },
+      { '<leader>0', '<cmd>Grapple select index=5 scope=global <cr>', desc = 'Session Global [5]' },
+    },
+  },
+}
